a16df25748ed7b9f82e3ca6407098da10592b9c0
Preesha Sinha <preesha2005@gmail.com>
2026-05-04 20:42:18 +0530

    Add: coin-change - submission-3

---
+++ Data Structures & Algorithms/coin-change/submission-3.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+    int dp[] = new int[amount + 1];
+    Arrays.fill(dp, amount + 1);
+
+    dp[0] = 0;
+
+    for (int coin : coins) {
+        for (int j = coin; j <= amount; j++) {
+            dp[j] = Math.min(dp[j], 1 + dp[j - coin]);
+        }
+    }
+
+    return dp[amount] > amount ? -1 : dp[amount];
+    }
+}          
